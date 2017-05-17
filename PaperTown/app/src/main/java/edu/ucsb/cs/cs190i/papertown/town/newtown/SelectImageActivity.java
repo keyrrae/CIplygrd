7e6105df204343b7b83cc2621c721c182d3ef09e
Zhenyu Yang <zcbmlijygrdwa@ymail.com>
2017-05-17 10:53:31 -0700

    fixed load image from IRU permission in selectImageActivity

--- PaperTown/app/src/main/java/edu/ucsb/cs/cs190i/papertown/town/newtown/SelectImageActivity.java
+++ PaperTown/app/src/main/java/edu/ucsb/cs/cs190i/papertown/town/newtown/SelectImageActivity.java
@@ -8,6 +8,9 @@
 
 package edu.ucsb.cs.cs190i.papertown.town.newtown;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -32,9 +35,9 @@ import edu.ucsb.cs.cs190i.papertown.R;
 import static android.provider.AlarmClock.EXTRA_MESSAGE;
 
 public class SelectImageActivity extends AppCompatActivity
-
 {
-
+    public static final int MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE = 112;
+    final int PICK_PHOTO_REQUEST = 5;
     Uri[] imageUris;
     final int NEW_PHOTO_REQUEST = 5;
     GridView grid;
@@ -89,10 +92,42 @@ public class SelectImageActivity extends AppCompatActivity
 
                     Log.i("addOnItemTouchListener", "last one!");
 
-                    //stat camera rool
-                    Intent pickPhoto = new Intent(Intent.ACTION_PICK,
-                            android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
-                    startActivityForResult(pickPhoto.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION) , NEW_PHOTO_REQUEST);//one can be replaced with any action code
+//                    //stat camera roll
+//                    Intent pickPhoto = new Intent(Intent.ACTION_PICK,
+//                            android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+//                    startActivityForResult(pickPhoto.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION) , NEW_PHOTO_REQUEST);//one can be replaced with any action code
+
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                        if (checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE)
+                                != PackageManager.PERMISSION_GRANTED) {
+
+
+                            // Should we show an explanation?
+                            if (shouldShowRequestPermissionRationale(
+                                    Manifest.permission.READ_EXTERNAL_STORAGE)) {
+                                // Explain to the user why we need to read the contacts
+                                Log.i("my", "permission.READ_EXTERNAL_STORAGE");
+
+                            }
+
+                            requestPermissions(new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
+                                    MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE);
+
+                            // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
+                            // app-defined int constant
+
+//                        return;
+                        }
+                        else{
+                            Log.i("my", "permission.READ_EXTERNAL_STORAGE3");
+                            //normal request goes here
+                            Intent pickPhoto = new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                            startActivityForResult(pickPhoto.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION), PICK_PHOTO_REQUEST);//one can be replaced with any action code
+                        }
+                    }
+
+
+
                 }
                 //Toast.makeText(MainActivity.this, "You Clicked at " +web[+ position], Toast.LENGTH_SHORT).show();
 
@@ -158,6 +193,36 @@ public class SelectImageActivity extends AppCompatActivity
             }
         }
 
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        Log.i("my", "permission requestCode = "+requestCode);
+        switch (requestCode) {
+            case MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    Log.i("my", "permission.READ_EXTERNAL_STORAGE2");
+
+                    //to start activity after the first time asking for permission
+                    Intent pickPhoto = new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                    startActivityForResult(pickPhoto.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION), PICK_PHOTO_REQUEST);//one can be replaced with any action code
+                    // permission was granted, yay! Do the
+                    // contacts-related task you need to do.
+
+                } else {
+                    Log.i("my", "permission.READ_EXTERNAL_STORAGE denied");
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                }
+//                return;
+            }
+
+            // other 'case' lines to check for other
+            // permissions this app might request
+        }
     }
 
     Uri[] addUri(Uri[] urilist, Uri in) {
--- PaperTown/app/src/main/java/edu/ucsb/cs/cs190i/papertown/town/newtown/SelelctImageGrid.java
+++ PaperTown/app/src/main/java/edu/ucsb/cs/cs190i/papertown/town/newtown/SelelctImageGrid.java
@@ -10,16 +10,26 @@
 
 package edu.ucsb.cs.cs190i.papertown.town.newtown;
 
+import android.Manifest;
 import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Color;
 import android.net.Uri;
+import android.os.Build;
+import android.support.v4.app.ActivityCompat;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.ImageView;
 
+import com.squareup.picasso.Picasso;
+
 import edu.ucsb.cs.cs190i.papertown.R;
+
+import static edu.ucsb.cs.cs190i.papertown.town.newtown.SelectImageActivity.MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE;
 
 /**
  * Created by Zhenyu on 2017-05-17.
@@ -30,7 +40,7 @@ public class SelelctImageGrid extends BaseAdapter {
     private final String[] web;
     private  int[] Imageid;
     private  Uri[] ImageUris;
-
+    ImageView imageView;
 
 
 
@@ -77,10 +87,70 @@ public class SelelctImageGrid extends BaseAdapter {
                 grid = new View(mContext);
                 grid = inflater.inflate(R.layout.grid_single_newtown_selectimage, null);
                 //TextView textView = (TextView) grid.findViewById(R.id.grid_text);
-                ImageView imageView = (ImageView) grid.findViewById(R.id.grid_image);
+                imageView = (ImageView) grid.findViewById(R.id.grid_image);
                 //textView.setText(web[position]);
                 //imageView.setImageResource(Imageid[position]);
-                imageView.setImageURI(ImageUris[position]);
+                //imageView.setImageURI(ImageUris[position]);
+                //imageView.setImageDrawable(mContext.getResources().getDrawable(R.drawable.test));
+                Log.i("addOnItemTouchListener", "ImageUris["+position+"] = "+ImageUris[position]);
+
+
+
+
+
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                    if (mContext.checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE)
+                            != PackageManager.PERMISSION_GRANTED) {
+
+
+                        // Should we show an explanation?
+                        if (ActivityCompat.shouldShowRequestPermissionRationale((SelectImageActivity)mContext,
+                                Manifest.permission.READ_EXTERNAL_STORAGE)) {
+                            // Explain to the user why we need to read the contacts
+                            Log.i("my", "permission.READ_EXTERNAL_STORAGE");
+
+                        }
+
+                        ActivityCompat.requestPermissions((SelectImageActivity)mContext,new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
+                                MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE);
+
+                        // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
+                        // app-defined int constant
+
+//                        return;
+                    }
+                    else{
+                        Log.i("my", "permission.READ_EXTERNAL_STORAGE3");
+                        //normal request goes here
+                        imageView.setImageURI(ImageUris[position]);
+//                Picasso.with(mContext).load(ImageUris[position])
+//                        .resize(20,20)
+//                        .fit()
+//                        .into(imageView);
+                    }
+                }
+
+
+
+
+
+
+
+
+//                imageView.setImageURI(ImageUris[position]);
+////                Picasso.with(mContext).load(ImageUris[position])
+////                        .resize(20,20)
+////                        .fit()
+////                        .into(imageView);
+
+
+
+
+
+
+
+
+
             }
             else{
                 grid = new View(mContext);
@@ -98,4 +168,34 @@ public class SelelctImageGrid extends BaseAdapter {
 
         return grid;
     }
+
+//    @Override
+//    public void onRequestPermissionsResult(int requestCode,
+//                                           String permissions[], int[] grantResults) {
+//        Log.i("my", "permission requestCode = "+requestCode);
+//        switch (requestCode) {
+//            case MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE: {
+//                // If request is cancelled, the result arrays are empty.
+//                if (grantResults.length > 0
+//                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+//                    Log.i("my", "permission.READ_EXTERNAL_STORAGE2");
+//                    //normal request goes here
+//                    imageView.setImageURI(ImageUris[position]);
+////                Picasso.with(mContext).load(ImageUris[position])
+////                        .resize(20,20)
+////                        .fit()
+////                        .into(imageView);
+//
+//                } else {
+//                    Log.i("my", "permission.READ_EXTERNAL_STORAGE denied");
+//                    // permission denied, boo! Disable the
+//                    // functionality that depends on this permission.
+//                }
+////                return;
+//            }
+//
+//            // other 'case' lines to check for other
+//            // permissions this app might request
+//        }
+//    }
 }
