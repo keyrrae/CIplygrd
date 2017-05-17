6797177f28df4418b44fe1b49b092ad74a22a112
Zhenyu Yang <zcbmlijygrdwa@ymail.com>
2017-05-17 11:52:56 -0700

    passed image Uris from NewTown Activity to TownDetailActivity, displayed them in GridView and ImageView

--- PaperTown/app/src/main/java/edu/ucsb/cs/cs190i/papertown/town/towndetail/TownDetailActivity.java
+++ PaperTown/app/src/main/java/edu/ucsb/cs/cs190i/papertown/town/towndetail/TownDetailActivity.java
@@ -15,6 +15,7 @@ import android.util.Log;
 import android.widget.GridView;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.squareup.picasso.Picasso;
 
@@ -34,7 +35,8 @@ public class TownDetailActivity extends AppCompatActivity {
   String category = "";
   String description = "";
   String information = "";
-//  Uri[] uriList;
+  ArrayList<String> uriStringArrayList;
+
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
@@ -47,8 +49,8 @@ public class TownDetailActivity extends AppCompatActivity {
 
     this.imageGrid = (GridView) findViewById(R.id.detail_image_grid);
     this.uriList = new ArrayList<Uri>();
-    this.uriList.add(Uri.parse("https://s-media-cache-ak0.pinimg.com/564x/8f/af/c0/8fafc02753b860c3213ffe1748d8143d.jpg"));
-    this.uriList.add(Uri.parse("https://s-media-cache-ak0.pinimg.com/564x/8f/af/c0/8fafc02753b860c3213ffe1748d8143d.jpg"));
+//    this.uriList.add(Uri.parse("https://s-media-cache-ak0.pinimg.com/564x/8f/af/c0/8fafc02753b860c3213ffe1748d8143d.jpg"));
+//    this.uriList.add(Uri.parse("https://s-media-cache-ak0.pinimg.com/564x/8f/af/c0/8fafc02753b860c3213ffe1748d8143d.jpg"));
 
     String s = getIntent().getStringExtra("dataToD");
     Log.i("dataToD", "data = "+s);
@@ -58,6 +60,19 @@ public class TownDetailActivity extends AppCompatActivity {
     description = getIntent().getStringExtra("description");
     category = getIntent().getStringExtra("category");
     information = getIntent().getStringExtra("information");
+    uriStringArrayList = getIntent().getStringArrayListExtra("uriStringArrayList");
+
+    //process uriStringArrayList, put data into uriList
+    if(uriStringArrayList!=null&&uriStringArrayList.size()>0) {
+      for (int i = 0; i < uriStringArrayList.size(); i++) {
+        uriList.add(Uri.parse(uriStringArrayList.get(i)));
+      }
+    }
+    else{
+      Toast.makeText(getApplicationContext(), "Cannot get images, default images used!", Toast.LENGTH_SHORT).show();
+      this.uriList.add(Uri.parse("https://s-media-cache-ak0.pinimg.com/564x/8f/af/c0/8fafc02753b860c3213ffe1748d8143d.jpg"));
+      this.uriList.add(Uri.parse("https://s-media-cache-ak0.pinimg.com/564x/8f/af/c0/8fafc02753b860c3213ffe1748d8143d.jpg"));
+    }
 
     //load title
     if(title!=null) {
@@ -90,7 +105,7 @@ public class TownDetailActivity extends AppCompatActivity {
     }
 
 
-    //load images
+    //load uriStringArrayList
     if(uriList!=null) {
       if(uriList.size()>0) {
         ImageView detail_town_image = (ImageView) findViewById(R.id.detail_town_image);
@@ -100,5 +115,10 @@ public class TownDetailActivity extends AppCompatActivity {
         this.imageGrid.setAdapter(new ImageAdapter(this, this.uriList));
       }
     }
+
+
+
+
+
   }
 }
