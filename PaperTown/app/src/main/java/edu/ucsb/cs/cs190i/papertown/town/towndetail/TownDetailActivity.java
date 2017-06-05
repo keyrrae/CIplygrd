c2044735852fa893fd471a6e1ae95b052021e040
Zhenyu Yang <zcbmlijygrdwa@ymail.com>
2017-06-04 19:21:46 -0700

    Add remove town from draft after posting it to server

--- PaperTown/app/src/main/java/edu/ucsb/cs/cs190i/papertown/town/towndetail/TownDetailActivity.java
+++ PaperTown/app/src/main/java/edu/ucsb/cs/cs190i/papertown/town/towndetail/TownDetailActivity.java
@@ -79,8 +79,11 @@ import edu.ucsb.cs.cs190i.papertown.R;
 import edu.ucsb.cs.cs190i.papertown.TownMapIcon;
 import edu.ucsb.cs.cs190i.papertown.models.Town;
 import edu.ucsb.cs.cs190i.papertown.models.TownBuilder;
+import edu.ucsb.cs.cs190i.papertown.models.TownRealm;
 import edu.ucsb.cs.cs190i.papertown.models.UserSingleton;
 import edu.ucsb.cs.cs190i.papertown.town.newtown.myMapFragment;
+import io.realm.Realm;
+import io.realm.RealmResults;
 
 
 public class TownDetailActivity extends AppCompatActivity {
@@ -247,6 +250,15 @@ public class TownDetailActivity extends AppCompatActivity {
                                                             "Successfully submitted network",
                                                             Toast.LENGTH_SHORT
                                                     ).show();
+
+                                                    Realm.getInstance(getApplicationContext()).executeTransaction(new Realm.Transaction() {
+                                                        @Override
+                                                        public void execute(Realm realm) {
+                                                            RealmResults<TownRealm> result = realm.where(TownRealm.class).equalTo("townId",passedInTown.getId()).findAll();
+                                                            result.clear();
+                                                        }
+                                                    });
+
                                                     finish();
                                                 }
                                             });
