3e5158b66e5469366f0ef780f65112ad5d5fc1ab
Zhenyu Yang <zcbmlijygrdwa@ymail.com>
2017-06-08 23:39:18 -0700

    Add file compression in TownDetailActivity upload image function

--- PaperTown/app/src/main/java/edu/ucsb/cs/cs190i/papertown/town/towndetail/TownDetailActivity.java
+++ PaperTown/app/src/main/java/edu/ucsb/cs/cs190i/papertown/town/towndetail/TownDetailActivity.java
@@ -464,9 +464,23 @@ public class TownDetailActivity extends AppCompatActivity {
             if (resultCode == RESULT_OK ) {
                 List<Uri> mSelected = Matisse.obtainResult(intent);
                 List<String> uris = new ArrayList<>();
-                for (int i = 0; i < mSelected.size(); i++) {
-                    uris.add(mSelected.get(i).toString());
+//                for (int i = 0; i < mSelected.size(); i++) {
+//
+//
+//
+//
+//
+//                    uris.add(mSelected.get(i).toString());
+//                }
+
+                //file compression
+                for(int i=0; i<mSelected.size(); i++){
+                    File f = new File(mSelected.get(i).toString());
+                    f = new File(resizeAndCompressImageBeforeSend(getApplicationContext(), mSelected.get(i), "/" + f.getName() + UUID.randomUUID().toString() + System.currentTimeMillis() + ".jpg"));
+                    Uri uri = (Uri.fromFile(f));
+                    uris.add(uri.toString());
                 }
+
 //                for(int i=0; i<mSelected.size(); i++){
 //                    uriList.add(mSelected.get(i));
 //                }
@@ -476,6 +490,115 @@ public class TownDetailActivity extends AppCompatActivity {
             }
             if (resultCode == Activity.RESULT_CANCELED) {
                 Log.i("onActivityResult", "RESULT_CANCELED");
+            }
+        }
+    }
+
+    public String resizeAndCompressImageBeforeSend(Context context, Uri inputUri, String fileName) {
+        String filePath = getRealPathFromURI2(context, inputUri);
+        //String filePath = inputUri.toString();
+        final int MAX_IMAGE_SIZE = 600 * 800; // max final file size in kilobytes   700 * 1024;
+
+        // First decode with inJustDecodeBounds=true to check dimensions of image
+        final BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inJustDecodeBounds = true;
+        BitmapFactory.decodeFile(filePath, options);
+
+        // Calculate inSampleSize(First we are going to resize the image to 800x800 image, in order to not have a big but very low quality image.
+        //resizing the image will already reduce the file size, but after resizing we will check the file size and start to compress image
+        options.inSampleSize = calculateInSampleSize(options, 800, 800);
+
+        // Decode bitmap with inSampleSize set
+        options.inJustDecodeBounds = false;
+        options.inPreferredConfig = Bitmap.Config.ARGB_8888;
+
+        Bitmap bmpPic = BitmapFactory.decodeFile(filePath, options);
+
+
+        int compressQuality = 100; // quality decreasing by 5 every loop.
+        int streamLength;
+        do {
+            ByteArrayOutputStream bmpStream = new ByteArrayOutputStream();
+            Log.d("compressBitmap", "Quality: " + compressQuality);
+            bmpPic.compress(Bitmap.CompressFormat.JPEG, compressQuality, bmpStream);
+            byte[] bmpPicByteArray = bmpStream.toByteArray();
+            streamLength = bmpPicByteArray.length;
+            compressQuality -= 5;
+            Log.d("compressBitmap", "Size: " + streamLength / 1024 + " kb");
+        } while (streamLength >= MAX_IMAGE_SIZE);
+
+        try {
+            //save the resized and compressed file to disk cache
+            Log.d("compressBitmap", "cacheDir: " + context.getCacheDir());
+            FileOutputStream bmpFile = new FileOutputStream(context.getCacheDir() + fileName);
+            bmpPic.compress(Bitmap.CompressFormat.JPEG, compressQuality, bmpFile);
+            bmpFile.flush();
+            bmpFile.close();
+        } catch (Exception e) {
+            Log.e("compressBitmap", "Error on saving file");
+        }
+        //return the path of resized and compressed file
+        return context.getCacheDir() + fileName;
+    }
+
+    public static int calculateInSampleSize(BitmapFactory.Options options, int reqWidth, int reqHeight) {
+        String debugTag = "MemoryInformation";
+        final int height = options.outHeight;
+        final int width = options.outWidth;
+        Log.d(debugTag, "image height: " + height + "---image width: " + width);
+        int inSampleSize = 1;
+
+        if (height > reqHeight || width > reqWidth) {
+
+            final int halfHeight = height / 2;
+            final int halfWidth = width / 2;
+
+            // Calculate the largest inSampleSize value that is a power of 2 and keeps both
+            // height and width larger than the requested height and width.
+            while ((halfHeight / inSampleSize) > reqHeight && (halfWidth / inSampleSize) > reqWidth) {
+                inSampleSize *= 2;
+            }
+        }
+        Log.d(debugTag, "inSampleSize: " + inSampleSize);
+        return inSampleSize;
+    }
+
+    public String getRealPathFromURI(Context context, Uri uri) {
+        String filePath = "";
+        String wholeID = DocumentsContract.getDocumentId(uri);
+
+        // Split at colon, use second item in the array
+        String id = wholeID.split(":")[1];
+
+        String[] column = {MediaStore.Images.Media.DATA};
+
+        // where id is equal to
+        String sel = MediaStore.Images.Media._ID + "=?";
+
+
+        Cursor cursor = context.getContentResolver().query(MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+                column, sel, new String[]{id}, null);
+
+        int columnIndex = cursor.getColumnIndex(column[0]);
+
+        if (cursor.moveToFirst()) {
+            filePath = cursor.getString(columnIndex);
+        }
+        cursor.close();
+        return filePath;
+    }
+
+    public String getRealPathFromURI2(Context context, Uri contentUri) {
+        Cursor cursor = null;
+        try {
+            String[] proj = { MediaStore.Images.Media.DATA };
+            cursor = context.getContentResolver().query(contentUri,  proj, null, null, null);
+            int column_index = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+            cursor.moveToFirst();
+            return cursor.getString(column_index);
+        } finally {
+            if (cursor != null) {
+                cursor.close();
             }
         }
     }
